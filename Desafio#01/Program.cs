b8f261af8786bf2670383871bf0953792954ebbd
Rhayssa Kramer <112133224+rhayssakramer@users.noreply.github.com>
2024-10-01 09:54:18 -0300

    Desafio#01

---
+++ Desafio#01/Program.cs
@@ -0,0 +1,20 @@
+﻿/*using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("PROD = " + (        )); //implemente o código que representa a multiplicação.
+            Console.ReadLine();
+        }
+    }
+*/
+
+int a, b;
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("PROD = " + (a * b)); //implemente o código que representa a multiplicação.
+Console.ReadLine();
